f20b002bf10f7da15048b022a6ce66b9fb1d309f
Mohammad-Hakemi22 <hakemi.mohammad.22@gmail.com>
2022-07-01 20:40:56 +0430

    update cache in background. it's making the server fucking faster

--- main.go
+++ main.go
@@ -32,9 +32,29 @@ func main() {
 }
 
 func handler(numStories int, tpl *template.Template) http.HandlerFunc {
+	sc := stroryCache{ // created here (not inside handlerfunc) because we won't make cache for each request
+		numStories: numStories,
+		duration:   6 * time.Second,
+	}
+
+	go func(){
+		ticker := time.NewTicker(3 * time.Second)
+		for {
+			temp := stroryCache{
+				numStories: numStories,
+				duration:   6 * time.Second,
+			}
+			temp.stories()
+			sc.mutex.Lock()
+			sc.cache = temp.cache
+			sc.expiration = temp.expiration
+			sc.mutex.Unlock()
+			<-ticker.C
+		}
+	}()
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		stories, err := getCachedStories(numStories)
+		stories, err := sc.stories()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -51,25 +71,27 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	})
 }
 
-var (
-	cache           []item
-	cacheExpiration time.Time
-	cacheMutex      sync.Mutex
-)
+type stroryCache struct {
+	numStories int
+	cache      []item
+	expiration time.Time
+	duration   time.Duration
+	mutex      sync.Mutex
+}
 
-func getCachedStories(numStories int) ([]item, error) {
-	cacheMutex.Lock()
-	defer cacheMutex.Unlock()
-	if time.Since(cacheExpiration) < 0 {
-		return cache, nil
+func (sc *stroryCache) stories() ([]item, error) {
+	sc.mutex.Lock()
+	defer sc.mutex.Unlock()
+	if time.Since(sc.expiration) < 0 {
+		return sc.cache, nil
 	}
-	stories, err := getTopStories(numStories)
+	stories, err := getTopStories(sc.numStories)
 	if err != nil {
 		return nil, err
 	}
-	cache = stories
-	cacheExpiration = time.Now().Add(1 * time.Minute)
-	return cache, nil
+	sc.expiration = time.Now().Add(sc.duration)
+	sc.cache = stories
+	return sc.cache, nil
 }
 
 func getTopStories(numStories int) ([]item, error) {
